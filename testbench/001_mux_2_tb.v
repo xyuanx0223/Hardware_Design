66d5f221630262ca8d021ca520f41137055b8498
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-06 16:38:54 -0600

    Create 001_mux_2_tb.v

---
+++ testbench/001_mux_2_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ns
+
+module mux_2_tb();
+  reg  s_a, s_b, sel;
+  wire out;
+
+  mux_2 mux_2_inst(
+    .a(s_a),
+    .b(s_b),
+    .sel(sel),
+    .out(out)
+  );
+  initial begin
+    s_a = 0; s_b = 0; sel = 0;
+    #200
+    s_a = 0; s_b = 0; sel = 1;
+    #200
+    s_a = 0; s_b = 1; sel = 0;
+    #200
+    s_a = 0; s_b = 1; sel = 1;
+    #200
+    s_a = 1; s_b = 0; sel = 0;
+    #200
+    s_a = 1; s_b = 0; sel = 1;
+    #200
+    s_a = 1; s_b = 1; sel = 0;
+    #200
+    s_a = 1; s_b = 1; sel = 1;
+  end
+endmodule
