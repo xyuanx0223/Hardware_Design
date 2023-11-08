6ede884083ba5815b9e2cb45d0d9beed181b1252
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-08 14:05:38 -0600

    Create local.sv

---
+++ systemverilog/Class/local.sv
@@ -0,0 +1,34 @@
+//`timescale 1ns/1ps
+
+class data_hide;
+  // local modifier to hide the data
+  local bit [3: 0] data = 4'b1111;
+  
+  function void data_hide_value();
+    $display("----------------------");
+    $display("value of the dsata: %0b", this.data);
+    $display("----------------------");    
+  endfunction
+  
+endclass
+ 
+module tb();
+  
+  data_hide d1; // handler
+  bit [3: 0] dr;
+  
+  initial begin
+    d1 = new();
+    #10;
+    d1.data_hide_value();
+  end
+  
+
+  initial begin
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
+    #200;
+    $finish();
+  end
+  
+endmodule
