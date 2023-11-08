d47123610f6803a20222d99b9c241bec029d10f5
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-08 15:18:28 -0600

    Update 002_randc&rand.sv

--- systemverilog/Transaction & Generator/002_randc&rand.sv
+++ systemverilog/Transaction & Generator/002_randc&rand.sv
@@ -16,15 +16,14 @@ module tb();
   integer i;
   
   initial begin
+    $display("--------------------");
     t1 = new();
     for (i = 0; i <20; i++) begin
       t1.randomize();
       #10;
-      $display("--------------------");
-      $display("value of a: %0d", t1.a);
-      $display("--------------------");
+      $display("value of a: %0d at index: %0d", t1.a, i);
     end
-    
+    $display("--------------------");
     $dumpfile("dump.vcd"); 
     $dumpvars;
     #200;
