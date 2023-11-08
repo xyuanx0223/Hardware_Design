0a3baaebbf3f079c8ae1c2780004ffbd4020816a
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-08 16:31:35 -0600

    Update 3_event.sv

--- Systemverilog/02_Interprocess Communication/3_event.sv
+++ Systemverilog/02_Interprocess Communication/3_event.sv
@@ -1,1 +1,32 @@
+//`timescale 1ns/1ps
 
+event a;
+
+module tb();
+
+  initial begin
+    $display("-------------------------");
+    fork
+    // process 1
+    begin
+      // trigger event notion ->
+      #10;    
+      -> a;
+    end
+    // process 2
+    begin
+      @(a.triggered);
+      $display("Event Triggered at %0t", $time);
+    end
+
+    join
+    $display("All the process executed successfully at %0t", $time);  
+    
+    $display("-------------------------");
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
+    #100;
+    $finish();
+  end
+  
+endmodule
