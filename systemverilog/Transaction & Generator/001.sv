0365330620a21fcf85c519bc2c1d73d6772c131e
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-08 13:28:50 -0600

    Create 001.sv

---
+++ systemverilog/Transaction & Generator/001.sv
@@ -0,0 +1,30 @@
+class first;
+  
+  bit [7: 0] data = 8'hff;
+  
+endclass
+
+module tb();
+  
+  first f1;
+  bit [7: 0] datarcvd;
+  
+  initial begin
+    f1 = new();
+    
+    datarcvd = f1.data;
+    #10;
+    $display("----------------");
+    $display("value rcvd: %0x", datarcvd);
+    $display("----------------");
+  end
+  
+
+  initial begin
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
+    #200;
+    $finish();
+  end
+  
+endmodule
