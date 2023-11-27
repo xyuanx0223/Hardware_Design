2a7747b81525d36646b19826c74ae5ef4e631eb4
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-27 16:57:34 -0600

    Create Behavioral_Counter.v

---
+++ Verilog/Modeling_Style/Behavioral_Counter.v
@@ -0,0 +1,18 @@
+module counter
+(
+input clk, rst,///active high
+input ld,
+input [3:0] ldvalue,
+output [3:0] dout 
+);
+  reg [3:0] temp;
+  always @ (posedge clk, posedge rst) begin
+    if (rst) temp <= 4'b0;
+    else begin
+      if(ld) temp <= ldvalue;
+      else temp <= temp + 4'b1;
+    end
+  end
+  
+  assign temp = dout;
+endmodule
