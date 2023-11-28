823b9378d4b17bd043d320b41375c7472061d030
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-28 15:27:11 -0600

    Update Gate_level_Full_Adder.v

--- Verilog/Modeling_Style/Gate_level_Full_Adder.v
+++ Verilog/Modeling_Style/Gate_level_Full_Adder.v
@@ -4,9 +4,16 @@ module fa(
 );
   wire t1, t2, t3;
   // wire [3:0] temp;
+  /*
   assign t1 = a ^ b;    
   assign t2 = t1 & cin;  
   assign t3 = a & b;  
   assign s  = t1 ^ cin;  
   assign cout = t2 | t3;
+  */
+  xor x1 (t1, a, b);
+  xor x2 (s, t1, cin);
+  and a1 (t2, cin, t1);
+  and a2 (t3, a, b);
+  or  o1 (cout, t2, t3);
 endmodule
