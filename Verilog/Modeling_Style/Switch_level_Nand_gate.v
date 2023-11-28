b9046014ee6f3ed62d7c6263534544b7832201f5
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-28 17:14:16 -0600

    Update Switch_level_Nand_gate.v

--- Verilog/Modeling_Style/Switch_level_Nand_gate.v
+++ Verilog/Modeling_Style/Switch_level_Nand_gate.v
@@ -30,3 +30,26 @@ module and2(
   nmos n2(c, gnd, b);
   
 endmodule
+
+/*And_gate
+module nand2 (
+  input a,b,
+  output y
+);
+ 
+supply1 vdd;
+supply0 gnd;
+wire t1;
+wire t2;
+ 
+pmos p1 (t2, vdd, a);
+pmos p2 (t2, vdd, b);
+ 
+nmos n1 (t2, t1, a);
+nmos n2 (t1, gnd, b);
+ 
+pmos p3 (y, vdd, t2);
+nmos n3 (y, gnd, t2);
+ 
+endmodule 
+*/
