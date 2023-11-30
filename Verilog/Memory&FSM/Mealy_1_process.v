7b5e3de9e2c7408796793bef0712ba770f6329b9
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-11-30 15:57:39 -0600

    Create Mealy_1_process.v

---
+++ Verilog/Memory&FSM/Mealy_1_process.v
@@ -0,0 +1,62 @@
+module fsm(
+    input clk, rst, din,
+    output reg dout
+    );
+parameter idle = 0;
+parameter s0 = 1;
+parameter s1 = 2; 
+ 
+reg [1:0] state = idle; 
+  
+always@(posedge clk)
+begin
+   if(rst == 1'b1)
+     begin
+     state <= idle;
+     dout  <= 1'b0;
+     end
+   else 
+   begin
+      case(state)
+       
+       idle: begin
+                dout <= 1'b0;
+                state <= s0;
+                
+       end
+
+       s0: begin 
+                 if(din)
+                 begin
+                  dout   <= 1'b1;
+                  state  <= s1; 
+                 end
+                else
+                 begin
+                  dout  <= 1'b0;
+                  state <= s0;
+                end 
+      end
+       
+      s1: 
+      begin
+                 if(din)
+                 begin
+                  dout   <= 1'b0;
+                  state  <= s0; 
+                 end
+                else
+                 begin
+                  dout  <= 1'b0;
+                  state <= s1;
+                 end 
+      end
+       
+      default: begin
+          state  <= idle;
+          dout   <=  1'b0;
+      end
+      endcase
+     end           
+end
+endmodule
