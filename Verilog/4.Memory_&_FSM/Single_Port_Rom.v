a789558d12854d53c432351cb0bbd6b503970899
Jonathan Xu <92006744+xyuanx0223@users.noreply.github.com>
2023-12-05 15:51:20 -0600

    Create Single_Port_Rom.v

---
+++ Verilog/4.Memory_&_FSM/Single_Port_Rom.v
@@ -0,0 +1,9 @@
+// Matlab Code
+// t = 0: 0.001:1;
+// x = 5 * sin(2 * pi * 50 * t ) + 5;
+// plot(t,x);
+// Y = cast(x,'uint8');
+// Hd = dfilt.dffir(Y);
+// Hd.Arithmetic = 'fixed';
+// Hd.CoeffWordLength = 8;
+// coewrite(Hd, 10, 'mycoefile');
